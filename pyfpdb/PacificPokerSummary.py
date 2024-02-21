a0d3f9e90b7a95368757a88df450411c6a00a121
Chaz Littlejohn <chaz.littlejohn@gmail.com>
2024-02-21 13:43:10 -0500

    Fixed support for 888 tourney summaries

--- pyfpdb/PacificPokerSummary.py
+++ pyfpdb/PacificPokerSummary.py
@@ -44,7 +44,7 @@ class PacificPokerSummary(TourneySummary):
                            'NUM' : u".,\d\xa0"                     # legal characters in number format
                     }
     
-    re_Identify = re.compile(u'\*{5}\s(Cassava|888poker|888)(\.[a-z]{2})?(\-[a-z]{2})? Tournament Summary\s\*{5}')
+    re_Identify = re.compile(u'\*{5}\s(Cassava|888poker|888)?(\.[a-z]{2})?(\-[a-z]{2})? ?Tournament Summary\s\*{5}')
     
     re_TourneyInfo = re.compile(u"""
                         Tournament\sID:\s(?P<TOURNO>[0-9]+)\s+
